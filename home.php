5b05b8397636cb8ab3c3c01b4c9bc0ce7eebddb1
NiekKrammer <niekkrammer@gmail.com>
2024-06-12 09:28:38 +0200

    New Styles

--- home.php
+++ home.php
@@ -120,41 +120,14 @@
 
 	</section>
 
-	<script>
-		// Toon/verberg cargo lading (kg) veld
-		const vliegtuigTypes = document.querySelector('.vliegtuigTypes');
-		const cargo_lading_input = document.querySelectorAll('.cargo_lading');
+	<footer>
+		<p>© 2024 MboAir</p>
+	</footer>
 
-		vliegtuigTypes.addEventListener('change', function handleChange(event) {
-			const selectedValue = event.target.value;
-			cargo_lading_input.forEach(function(field) {
-				if (selectedValue === 'Boeing 737-700') {
-					field.style.display = 'block';
-				} else {
-					field.style.display = 'none';
-				}
-			});
-		});
-
-		// Vertrek moet tussen 06:00 uur en 16:00 uur zijn
-		const vertrekTijd = document.querySelector(".vertrek_tijd");
-
-		vertrekTijd.addEventListener("input", function() {
-			let selectedTime = vertrekTijd.value;
-			let minTime = "06:00";
-			let maxTime = "16:00";
-
-			if (selectedTime < minTime || selectedTime > maxTime) {
-				vertrekTijd.setCustomValidity("Tijd moet tussen 06:00 en 16:00 liggen.");
-			} else {
-				vertrekTijd.setCustomValidity("");
-			}
-		});
+	
+	<script src="script.js">
 	</script>
 
-	<footer>
-		© 2024 MboAir
-	</footer>
 </body>
 
 </html>
